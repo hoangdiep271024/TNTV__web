abee89fa8c578fa7c81b782a726768ad983491f1
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2024-10-30 23:36:23 +0700

    diep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import Auth from "./components/page/Auth";
 import FilmDetail from "./components/film/FilmDetail";
 import FilmTime from "./components/film/FilmTime";
 import Lich_chieu from "./components/page/Lich_chieu";
-
+import FilmComment from "./components/film/FilmComment";
+import FilmBuy from "./components/film/FilmBuy";
+import Cinema from "./components/page/Cinema";
 function App() {
   return (
     <Router>
@@ -14,6 +16,9 @@ function App() {
         <Route path="/phim/:film_name" element={<FilmDetail />}/>
         <Route path="/lich_chieu/:film_name" element={<FilmTime />}/>
         <Route path="/lich_chieu" element={<Lich_chieu />}/>
+        <Route path="/danh_gia/:film_name" element={<FilmComment/>}/>
+        <Route path="/mua_ve/:film_name" element={<FilmBuy/>}/>
+        <Route path="/rap" element={<Cinema/>}/>
       </Routes>
     </Router>);
 
---
+++ frontend/src/components/page/Cinema.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Shared from '../Shared'
+
+export default function Cinema() {
+  return (
+    <>
+    <Shared></Shared>
+    </>
+  )
+}
