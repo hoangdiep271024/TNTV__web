905ead7f5658e6951d6f7da0c4006f5f9e71b7f4
22026536 <22026536>
2024-10-31 18:00:38 +0700

    quang

--- backend/api/user/muaVe.js
+++ backend/api/user/muaVe.js
@@ -1,5 +1,5 @@
 import express from "express";
-import showSeat from "../../controllers/user/QRPayment.js";
+import { showSeat } from "../../controllers/user/muaVe.js";
 
 const routerMuaVe = express.Router()
 routerMuaVe.post("/showtime_id=:showtime_id",showSeat)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Auth from "./components/page/Auth";
 import FilmDetail from "./components/film/FilmDetail";
 import FilmTime from "./components/film/FilmTime";
+import BookingTicket from "./components/Mua_ve/index";
+import Auth from "./components/page/Auth";
 import Lich_chieu from "./components/page/Lich_chieu";
-
 function App() {
   return (
     <Router>
@@ -14,6 +14,7 @@ function App() {
         <Route path="/phim/:film_name" element={<FilmDetail />}/>
         <Route path="/lich_chieu/:film_name" element={<FilmTime />}/>
         <Route path="/lich_chieu" element={<Lich_chieu />}/>
+        <Route path="/mua_ve/:showtime_id" element = {<BookingTicket/>}/>
       </Routes>
     </Router>);
 
