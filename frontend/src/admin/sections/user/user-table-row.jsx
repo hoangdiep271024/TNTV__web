cd8901e8cf5dd2b0a38b464dcf15373fe067eb0b
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2024-11-07 00:46:35 +0700

    user management page - list screen initialization, missing edit user screen

---
+++ frontend/src/admin/sections/user/user-table-row.jsx
@@ -0,0 +1,84 @@
+import { Avatar, Box, Checkbox, IconButton, MenuItem, menuItemClasses, MenuList, Popover, TableCell, TableRow } from "@mui/material";
+import { useCallback, useState } from "react";
+
+import { Label } from '../../components/label';
+import { Iconify } from '../../components/iconify'
+
+export function UserTableRow({ row, selected, onSelectRow }) {
+    const [openPopover, setOpenPopover] = useState(null);
+
+    const handleOpenPopover = useCallback((event) => {
+        setOpenPopover(event.currentTarget);
+    }, []);
+
+    const handleClosePopover = useCallback(() => {
+        setOpenPopover(null);
+    }, []);
+
+    return (
+        <>
+            <TableRow hover tabIndex={-1} role="checkbox" selected={selected}>
+                <TableCell padding="checkbox">
+                    <Checkbox disableRipple checked={selected} onChange={onSelectRow} />
+                </TableCell>
+
+                <TableCell component="th" scope="row">
+                    <Box gap={2} display="flex" alignItems="center">
+                        <Avatar alt={row.name} src={row.avatarUrl} />
+                        {row.name}
+                    </Box>
+                </TableCell>
+
+                <TableCell>{row.email}</TableCell>
+
+                <TableCell>{row.phonenumber}</TableCell>
+
+                <TableCell>{row.role}</TableCell>
+
+                <TableCell>
+                    <Label color={(row.status === "banned" && 'error') || 'success'}>{row.status}</Label>
+                </TableCell>
+
+                <TableCell align="right">
+                    <IconButton onClick={handleOpenPopover}>
+                        <Iconify icon="eva:more-vertical-fill" />
+                    </IconButton>
+                </TableCell>
+
+                <Popover
+                    open={Boolean(openPopover)}
+                    anchorEl={openPopover}
+                    onClose={handleClosePopover}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+                    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                >
+                    <MenuList
+                        disablePadding
+                        sx={{
+                            p: 0.5,
+                            gap: 0.5,
+                            width: 140,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            [`& .${menuItemClasses.root}`]: {
+                                px: 1,
+                                gap: 2,
+                                borderRadius: 0.75,
+                                [`&. ${menuItemClasses.selected}`]: { bgcolor: 'action.selected' },
+                            },
+                        }}
+                    >
+                        <MenuItem onClick={handleClosePopover}>
+                            <Iconify icon="solar:pen-bold" />
+                            Edit
+                        </MenuItem>
+                        <MenuItem onClick={handleClosePopover} sx={{ color: 'error.main' }}>
+                            <Iconify icon="solar:trash-bin-trash-bold" />
+                            Delete
+                        </MenuItem>
+                    </MenuList>
+                </Popover>
+            </TableRow>
+        </>
+    )
+}
