f510e59ef9cdec004d5b0c18d12f15de1789a84c
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2024-11-13 13:45:10 +0700

    diep

--- backend/api/user/cinema.js
+++ backend/api/user/cinema.js
@@ -3,4 +3,4 @@ import { cinema, cinemaID } from "../../controllers/user/cinema.js";
 const routerCinema = express.Router()
 routerCinema.post("/region_id=:region_id" ,cinema) 
 routerCinema.post("/:cinema_name")
-export default routerCinema;
+export default routerCinema
