05333f795bc26dc86f5767589e64c3b0bf7f9e27
hoangdiep271024 <144908726+hoangdiep271024@users.noreply.github.com>
2024-12-05 17:05:33 +0700

    diep

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,21 @@
 import cors from "cors";
 
-const whitelist = new Set(['http://example.com', 'https://example.org']);
-
+// const whitelist = new Set(['http://example.com', 'https://example.org']);
+const whitelist = new Set(['*']);
 const corOptions = {
     optionsSuccesStatus: 200,
     origin: function(origin,callback) {
-        if(whitelist.has(origin)) {
-            callback(null,true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
+        // if(whitelist.has(origin)) {
+        //     callback(null,true);
+        // } else {
+        //     callback(new Error("Not allowed by CORS"));
+        // }
+        callback(null,true)
     },
+
     credentials: true
 }
+
 
 export default cors(corOptions);
 
