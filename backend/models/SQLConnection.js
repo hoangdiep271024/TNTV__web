03213e92d13ad568e1b3652d7c15bec338cfd960
22026526-san <sangv6548@gmail.com>
2024-12-18 16:04:52 +0700

    fix

--- backend/models/SQLConnection.js
+++ backend/models/SQLConnection.js
@@ -5,8 +5,8 @@ const connection = await mysql.createConnection({
     host: 'localhost', // Địa chỉ của máy chủ cơ sở dữ liệu
     port: 3306,
     user: 'root', // Tên người dùng của cơ sở dữ liệu
-    password: 'Noname4ever@', // Mật khẩu của cơ sở dữ liệu
-    database: 'newschema' // Tên của cơ sở dữ liệu
+    password: '5ang15122003', // Mật khẩu của cơ sở dữ liệu
+    database: 'test' // Tên của cơ sở dữ liệu
 });
 // Kết nối đến cơ sở dữ liệu
 connection.connect((err) => {
@@ -18,3 +18,24 @@ connection.connect((err) => {
 });
 
 export default connection;
+// import mysql from 'mysql2';
+
+// // Tạo một kết nối đơn lẻ
+// const connection = await mysql.createConnection({
+//     host: '10.96.210.203', // Địa chỉ của máy chủ cơ sở dữ liệu
+//     port: 3306, // Cổng của MySQL
+//     user: 'fall2024c8g13', // Tên người dùng của cơ sở dữ liệu
+//     password: 'nhtt2024', // Mật khẩu của cơ sở dữ liệu
+//     database: 'fall2024c8g13_nhtt' // Tên của cơ sở dữ liệu
+// });
+
+// // Kết nối đến cơ sở dữ liệu
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('Connected as id ' + connection.threadId);
+// });
+
+// export default connection;
