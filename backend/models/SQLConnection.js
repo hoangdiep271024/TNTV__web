337f7426448a584e4134b0fd846da767198bff4f
22026526-san <sangv6548@gmail.com>
2024-12-18 16:26:55 +0700

    fix jwt myacc

--- backend/models/SQLConnection.js
+++ backend/models/SQLConnection.js
@@ -6,13 +6,8 @@ const connection = mysql.createConnection({
     host: 'localhost', // Địa chỉ của máy chủ cơ sở dữ liệu
     port: 3306,
     user: 'root', // Tên người dùng của cơ sở dữ liệu
-<<<<<<< HEAD
     password: '5ang15122003', // Mật khẩu của cơ sở dữ liệu
     database: 'test' // Tên của cơ sở dữ liệu
-=======
-    password: '27102004', // Mật khẩu của cơ sở dữ liệu
-    database: 'web_phim' // Tên của cơ sở dữ liệu
->>>>>>> 4c3aa04eea8b8c4451754abeee8f672a4cea6a06
 });
 // Kết nối đến cơ sở dữ liệu
 connection.connect((err) => {
@@ -24,8 +19,6 @@ connection.connect((err) => {
 });
 export default connection;
 
-<<<<<<< HEAD
-export default connection;
 // import mysql from 'mysql2';
 
 // // Tạo một kết nối đơn lẻ
@@ -37,20 +30,6 @@ export default connection;
 //     database: 'fall2024c8g13_nhtt' // Tên của cơ sở dữ liệu
 // });
 
-=======
-// deployment
-
-// import mysql from 'mysql2';
-// // Tạo một kết nối đơn lẻ
-// const connection = mysql.createConnection({
-//     host: '10.96.210.203', // Địa chỉ máy chủ MySQL
-//     port: 3306,                      // Cổng MySQL (thường là 3306)
-//     user: 'fall2024c8g13',           // Tên người dùng MySQL
-//     password: 'nhtt2024',            // Mật khẩu của người dùng
-//     database: 'fall2024c8g13_nhtt',   // Tên cơ sở dữ liệu
-//     connectTimeout: 10000 // Tăng thời gian timeout lên 10 giây
-// });
->>>>>>> 4c3aa04eea8b8c4451754abeee8f672a4cea6a06
 // // Kết nối đến cơ sở dữ liệu
 // connection.connect((err) => {
 //     if (err) {
@@ -60,8 +39,4 @@ export default connection;
 //     console.log('Connected as id ' + connection.threadId);
 // });
 
-<<<<<<< HEAD
 // export default connection;
-=======
-// export default connection;
->>>>>>> 4c3aa04eea8b8c4451754abeee8f672a4cea6a06
