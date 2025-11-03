d311b0680eb87c655bb338fff999fb6ec6db5552
Quang Sang <sangv6548@gmail.com>
2025-11-03 11:41:24 +0700

    feat: add docker for project

--- backend/models/SQLConnection.js
+++ backend/models/SQLConnection.js
@@ -3,11 +3,11 @@
 import mysql from 'mysql2';
 // Tạo một kết nối đơn lẻ
 const connection = mysql.createConnection({
-    host: 'localhost', // Địa chỉ của máy chủ cơ sở dữ liệu
+    host: 'mysql', // Địa chỉ của máy chủ cơ sở dữ liệu
     port: 3306,
     user: 'root', // Tên người dùng của cơ sở dữ liệu
     password: '5ang15122003', // Mật khẩu của cơ sở dữ liệu
-    database: 'test' // Tên của cơ sở dữ liệu
+    database: 'TNTV_web' // Tên của cơ sở dữ liệu
 });
 // Kết nối đến cơ sở dữ liệu
 connection.connect((err) => {
